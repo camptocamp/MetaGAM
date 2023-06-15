4e3cdeebcd3e7df1f978a134094c428d82433854
Amr HAMADEH <amr.hamadeh@grenoblealpesmetropole.fr>
2023-06-15 10:33:39 +0200

    Ajout fichier tests

---
+++ TestPy.py
@@ -0,0 +1,21 @@
+"""
+Ce module contient une fonction pour compter le nombre d'entités dans une couche vectorielle.
+"""
+from qgis.core import QgsVectorLayer
+
+def count_entities(layer: QgsVectorLayer) -> int:
+    """
+    Compte le nombre d'entités dans une couche vectorielle.
+
+    Args:
+        layer (QgsVectorLayer): La couche vectorielle à compter.
+
+    Returns:
+        int: Le nombre total d'entités dans la couche.
+    """
+    entity_count = 0
+
+    if layer.isValid():
+        entity_count = layer.featureCount()
+
+    return entity_count
