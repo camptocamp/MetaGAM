1668df5341989d6852a2ec88bc1c1cb8f09e61aa
HAMADEH AMR <amr.hamadeh@grenoblealpesmetropole.fr>
2023-06-20 15:37:01 +0000

    Update geonetworkTest.py

--- geonetworkTest.py
+++ geonetworkTest.py
@@ -1,50 +1,25 @@
-
 import unittest
-from unittest.mock import Mock, patch
-from geonetworkDate import getMetaDateGN
+from geonetworkDate import getMetaDateGN, connexionGeonetwork
 
 class TestGetMetaDateGN(unittest.TestCase):
-    @patch('geonetworkDate.connexionGeonetwork')
-    @patch('geonetworkDate.requests.get')
-    def test_getMetaDateGN(self,connexionGeonetwork_mock, requests_get_mock):
-        # Préparer les données de test
+
+    def test_getMetaDateGN(self):
         user = "TestCICD"
         password = "Git12345@"
         uuid = "6b37a124-1e66-468f-b295-32213a0a4de3"
-        
-        # Mock de la fonction connexionGeonetwork
-        connexionGeonetwork = Mock()
-        connexionGeonetwork.return_value = (True, "https://geonetworkt.grenoblealpesmetropole.fr/geonetwork", "test_token", {}, user, password, None)
-        
-        # Mock de la réponse de la requête
-        response_mock = Mock()
-        response_mock.status_code = 200
-        response_mock.text = '''
-            <root>
-                <element>
-                    <Date>2023-04-27</Date>
-                </element>
-            </root>
-        '''
-        
-        # Mock de la fonction requests.get
-        requests_get = Mock()
-        requests_get.return_value = response_mock
-        
-        # Exécuter la fonction à tester
+
+        def fake_connexionGeonetwork(user, password):
+            return (True, "https://geonetworkt.grenoblealpesmetropole.fr/geonetwork", "fake_token", {}, user, password, None)
+
+        original_connexionGeonetwork = connexionGeonetwork
+        connexionGeonetwork = fake_connexionGeonetwork
+
         date_publication = getMetaDateGN(user, password, uuid)
-        
-        # Vérifier les appels de fonctions
-        connexionGeonetwork.assert_called_once_with(user, password)
-        requests_get.assert_called_once_with(
-            "https://geonetworkt.grenoblealpesmetropole.fr/geonetwork/srv/api/records/6b37a124-1e66-468f-b295-32213a0a4de3/formatters/xml?addSchemaLocation=false&increasePopularity=false&approved=false",
-            headers={"Accept": "application/xml", "Content-Type": "application/xml"},
-            cookies={"XSRF-TOKEN": "test_token"},
-            auth=(user, password)
-        )
-        
-        # Vérifier le résultat
-        self.assertEqual(date_publication, "2023-04-27")
+
+        connexionGeonetwork = original_connexionGeonetwork
+
+        self.assertEqual(date_publication, "27-04-2023")
 
 if __name__ == '__main__':
     unittest.main()
+
