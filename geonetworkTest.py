811736319b8546632e20e331f5ebcc4e8ff5e3dc
HAMADEH AMR <amr.hamadeh@grenoblealpesmetropole.fr>
2023-06-21 12:24:40 +0000

    Update geonetworkTest.py

--- geonetworkTest.py
+++ geonetworkTest.py
@@ -1,5 +1,5 @@
 import unittest
-from geonetworkDate import connexionGeonetwork
+from geonetworkDate import connexionGeonetwork, getMetaDateGN
 from requests.structures import CaseInsensitiveDict
 
 class MyTestClass(unittest.TestCase):
@@ -7,6 +7,7 @@ class MyTestClass(unittest.TestCase):
         # Initialisation des objets nécessaires pour le test
         self.user = "TestCICD"
         self.password = "Git12345@"
+        self.uuid = "2aa70b3f-76bf-4524-9a56-099ae1acd8e2"
 
     def tearDown(self):
         # Nettoyage après le test si nécessaire
@@ -25,6 +26,12 @@ class MyTestClass(unittest.TestCase):
         self.assertIsInstance(result[4], str)  # Vérifie que le cinquième élément est une chaîne de caractères
         self.assertIsInstance(result[5], str)  # Vérifie que le sixième élément est une chaîne de caractères
         self.assertIsNone(result[6])  # Vérifie que le septième élément est None
+
+    def test_getMetaDateGN(self):
+            # Appel de la fonction à tester
+            date_publication = getMetaDateGN(self.user, self.password, self.uuid)
+
+            self.assertIsNotNone(date_publication)  # Vérifier que la date de publication n'est pas None qui veut dire que la fiche existe
 
 if __name__ == '__main__':
     unittest.main()
