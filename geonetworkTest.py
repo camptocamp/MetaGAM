dadf5d15d526ba2b99d3a2ef11296dde8c91594c
HAMADEH AMR <amr.hamadeh@grenoblealpesmetropole.fr>
2023-06-21 10:32:56 +0000

    Update geonetworkTest.py

--- geonetworkTest.py
+++ geonetworkTest.py
@@ -1,24 +1,29 @@
 import unittest
-from geonetworkDate import getMetaDateGN, connexionGeonetwork
+from geonetworkDate import connexionGeonetwork
 
-class TestGetMetaDateGN(unittest.TestCase):
+class MyTestClass(unittest.TestCase):
+    def setUp(self):
+        # Initialisation des objets nécessaires pour le test
+        self.user = "TestCICD"
+        self.password = "Git12345@"
 
-    def test_getMetaDateGN(self):
-        user = "TestCICD"
-        password = "Git12345@"
-        uuid = "6b37a124-1e66-468f-b295-32213a0a4de3"
+    def tearDown(self):
+        # Nettoyage après le test si nécessaire
+        pass
 
-        def fake_connexionGeonetwork(user, password):
-            return (True, "https://geonetworkt.grenoblealpesmetropole.fr/geonetwork", "fake_token", {}, user, password, None)
+    def test_connexionGeonetwork(self):
+        # Appel de la fonction à tester
+        result = connexionGeonetwork(self.user, self.password)
 
-        original_connexionGeonetwork = connexionGeonetwork
-        connexionGeonetwork = fake_connexionGeonetwork
-
-        date_publication = getMetaDateGN(user, password, uuid)
-
-        connexionGeonetwork = original_connexionGeonetwork
-
-        self.assertEqual(date_publication, "27-04-2023")
+        # Assertions pour vérifier le comportement attendu
+        self.assertEqual(len(result), 7)  # Vérifie le nombre de valeurs renvoyées
+        self.assertIsInstance(result[0], bool)  # Vérifie que le premier élément est un booléen
+        self.assertIsInstance(result[1], str)  # Vérifie que le deuxième élément est une chaîne de caractères
+        self.assertIsInstance(result[2], str)  # Vérifie que le troisième élément est une chaîne de caractères
+        self.assertIsInstance(result[3], dict)  # Vérifie que le quatrième élément est un dictionnaire
+        self.assertIsInstance(result[4], str)  # Vérifie que le cinquième élément est une chaîne de caractères
+        self.assertIsInstance(result[5], str)  # Vérifie que le sixième élément est une chaîne de caractères
+        self.assertIsNone(result[6])  # Vérifie que le septième élément est None
 
 if __name__ == '__main__':
     unittest.main()
